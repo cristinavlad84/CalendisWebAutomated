4d05f2eda2a959f8dfe3d8890dc43c8ff36e6585
cristinavlad84 <cristina.vlad@evozon.com>
2017-10-04 17:55:54 +0300

    rest assured mock test

---
+++ src/test/java/ro/evozon/steps/serenity/rest/RestSteps.java
@@ -0,0 +1,54 @@
+package ro.evozon.steps.serenity.rest;
+
+import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.Cookie;
+import ro.evozon.AbstractPage;
+
+import java.util.*;
+
+import static net.serenitybdd.rest.RestRequests.given;
+
+public class RestSteps {
+
+    AbstractPage abstractPage;
+
+    String baseUrl = "https://business2.calendis.ro";
+
+    public Set<Cookie> extractCookie(){
+        abstractPage.getDriver().get(baseUrl);
+        Set<Cookie> allMyCookies = abstractPage.getDriver().manage().getCookies();
+        return allMyCookies;
+    }
+
+    @Step
+    public Response registerNewUser() {
+            int id = Math.abs(new Random().nextInt());
+            Map<String, Object> bodyData = new HashMap<>();
+            bodyData.put("category", 7);
+            bodyData.put("name", "vvv");
+            bodyData.put("email", "alienvspredator@automation.33mail.com");
+            bodyData.put("phone", "74587458745");
+            bodyData.put("voucher", "");
+
+            Set<Cookie> allMyCookies = extractCookie();
+            //Cookies cookies = new Cookies();
+
+            Map<String, Cookie> cookieMap = new HashMap<String,Cookie>();
+        for (Cookie cookieNow:allMyCookies) {
+            System.out.println("----" + cookieNow.getName() + ":" + cookieNow.getValue());
+            cookieMap.put(cookieNow.getName(), cookieNow);
+        }
+
+           // Cookie cookieData = new Cookie();
+
+        Response response =  given().contentType("application/json").cookies(cookieMap).accept("*/*")
+                    .body(bodyData).log().body()
+                    .baseUri(baseUrl)
+                    .basePath("/user/subscribe")
+                    .when().post();
+
+        return response;
+    }
+}
--- src/test/java/ro/evozon/tests/BaseTest.java
+++ src/test/java/ro/evozon/tests/BaseTest.java
@@ -2,16 +2,18 @@ package ro.evozon.tests;
 
 import net.thucydides.core.annotations.Managed;
 
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import ro.evozon.tools.ConfigUtils;
 
 public class BaseTest {
 
 	@Managed(uniqueSession = true)
 	public WebDriver webdriver;
 
-	// @Before
-	// public void variableSetup() {
-	// System.setProperty("webdriver.chrome.driver",
-	// ConfigUtils.getChromePath());
-	// }
+	 @Before
+	 public void variableSetup() {
+	 System.setProperty("webdriver.chrome.driver",
+	 "/usr/local/calendis/CalendisWebAutomated/CalendisWebAutomated/src/test/resources/drivers/chromedriverlinux");
+	 }
 }
